614dca554df086d8370e1a7c4fd73b657e28e500
AndyRoldan <andy.guevara@catolica.edu.sv>
2024-04-20 14:27:49 -0600

    Agregar archivos de proyecto.

---
+++ L02P02_2019GR603/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02P02_2019GR603.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
